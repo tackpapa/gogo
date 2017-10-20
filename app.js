1d99f38ee199ec8969597da54f9e3137ebc9cd26
kiyoungtack <cance0311@gmail.com>
2017-10-20 23:39:03 +0900

    modfiy

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './public')));
 // app.use(sass({
 //   src: path.join(__dirname, './public'),
 //   dest: path.join(__dirname, './public')
